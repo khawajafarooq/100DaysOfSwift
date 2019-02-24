3c3261548d528bcbee7cf36e6c615cef21d3f0a0
Farooq <KHAH@danskebank.dk>
2019-02-24 09:12:35 +0100

    Day 1 progress 👨‍💼

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+// String
+var str = "Hello, playground"
+str = "Goodbye"
+
+// Int
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var strScore = "Your score was \(score)"
+var results = "The test results are here: \(strScore)"
+
+// Constants
+let taylor = "swift"
+// always starts with constants
+
+// Type annotations
+let message = "Hello, playground" // type inference
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
